d0009095d500d02aeae8e0f3a38dc87e0c163304
fernandoalcasan <fernandoalcasan@outlook.com>
2021-09-28 14:28:17 -0500

    Player jump with OverlapBox to check ground implemented

--- Assets/InputSystem/PlayerActions.cs
+++ Assets/InputSystem/PlayerActions.cs
@@ -22,16 +22,24 @@ public class @PlayerActions : IInputActionCollection, IDisposable
                     ""name"": ""Movement"",
                     ""type"": ""Value"",
                     ""id"": ""95b35d40-0a81-4d92-ab47-183396c59715"",
-                    ""expectedControlType"": ""Vector2"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": ""Normalize(min=-1,max=1)"",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""a8c037c0-75f7-45d8-9e6e-8d21deb2fb64"",
+                    ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
                 }
             ],
             ""bindings"": [
                 {
-                    ""name"": ""2D Vector"",
-                    ""id"": ""e98e7332-7cf3-444b-be3b-31db0f120ec8"",
-                    ""path"": ""2DVector"",
+                    ""name"": ""1D Axis"",
+                    ""id"": ""f88ef4c3-2845-46f8-a50e-20053b909cd2"",
+                    ""path"": ""1DAxis"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -40,30 +48,8 @@ public class @PlayerActions : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": false
                 },
                 {
-                    ""name"": ""up"",
-                    ""id"": ""fbe47b44-23c7-43be-b468-d09b9c137fbe"",
-                    ""path"": ""<Keyboard>/w"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""down"",
-                    ""id"": ""9dbee7f0-1d5c-40df-8302-0d4c351130df"",
-                    ""path"": ""<Keyboard>/s"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""left"",
-                    ""id"": ""46299a1b-ee5e-48eb-a5f0-7a4d86804ff2"",
+                    ""name"": ""negative"",
+                    ""id"": ""d0c668c5-a203-4a7e-90be-f1c820e77ec9"",
                     ""path"": ""<Keyboard>/a"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -73,8 +59,8 @@ public class @PlayerActions : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": true
                 },
                 {
-                    ""name"": ""right"",
-                    ""id"": ""06085ede-c24b-4987-8f7a-9b25a18e9c15"",
+                    ""name"": ""positive"",
+                    ""id"": ""f9bfad88-3d87-443b-981c-4e0dbb68a601"",
                     ""path"": ""<Keyboard>/d"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -82,6 +68,17 @@ public class @PlayerActions : IInputActionCollection, IDisposable
                     ""action"": ""Movement"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""cc7ad9d6-450c-4d91-b45b-7f2f36087923"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -91,6 +88,7 @@ public class @PlayerActions : IInputActionCollection, IDisposable
         // Player_Map
         m_Player_Map = asset.FindActionMap("Player_Map", throwIfNotFound: true);
         m_Player_Map_Movement = m_Player_Map.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Map_Jump = m_Player_Map.FindAction("Jump", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -141,11 +139,13 @@ public class @PlayerActions : IInputActionCollection, IDisposable
     private readonly InputActionMap m_Player_Map;
     private IPlayer_MapActions m_Player_MapActionsCallbackInterface;
     private readonly InputAction m_Player_Map_Movement;
+    private readonly InputAction m_Player_Map_Jump;
     public struct Player_MapActions
     {
         private @PlayerActions m_Wrapper;
         public Player_MapActions(@PlayerActions wrapper) { m_Wrapper = wrapper; }
         public InputAction @Movement => m_Wrapper.m_Player_Map_Movement;
+        public InputAction @Jump => m_Wrapper.m_Player_Map_Jump;
         public InputActionMap Get() { return m_Wrapper.m_Player_Map; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -158,6 +158,9 @@ public class @PlayerActions : IInputActionCollection, IDisposable
                 @Movement.started -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnMovement;
                 @Movement.performed -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnMovement;
                 @Movement.canceled -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnJump;
             }
             m_Wrapper.m_Player_MapActionsCallbackInterface = instance;
             if (instance != null)
@@ -165,6 +168,9 @@ public class @PlayerActions : IInputActionCollection, IDisposable
                 @Movement.started += instance.OnMovement;
                 @Movement.performed += instance.OnMovement;
                 @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
             }
         }
     }
@@ -172,5 +178,6 @@ public class @PlayerActions : IInputActionCollection, IDisposable
     public interface IPlayer_MapActions
     {
         void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player Properties")]
     [SerializeField]
     private float _speed;
-    private PlayerActions _playerActions;
+    [SerializeField]
+    private float _jumpPower;
+    [SerializeField] [Range(1f, 5f)]
+    private float _jumpFallGravityMultiplier;
+
+    [Header("Ground Check Properties")]
+    [SerializeField]
+    private float _groundCheckHeight;
+    [SerializeField]
+    private LayerMask _groundMask;
+    [SerializeField]
+    private float _disableGCTime;
+
+    //Help vars
+    private float _moveInput;
+    private Vector2 _boxCenter;
+    private Vector2 _boxSize;
+    private bool _jumping;
+    private float _initialGravityScale;
+    private bool _groundCheckEnabled = true;
+    private WaitForSeconds _wait;
+
+    //References
     private Rigidbody2D _rbody;
-    private Vector2 _moveInput;
+    private PlayerActions _playerActions;
+    private BoxCollider2D _collider;
 
     void Awake()
     {
@@ -17,6 +42,16 @@ public class Player : MonoBehaviour
         _rbody = GetComponent<Rigidbody2D>();
         if (_rbody is null)
             Debug.LogError("Rigidbody2D is NULL!");
+
+        _initialGravityScale = _rbody.gravityScale;
+
+        _collider = GetComponent<BoxCollider2D>();
+        if (_collider is null)
+            Debug.Log("BoxCollider2D is NULL!");
+
+        _wait = new WaitForSeconds(_disableGCTime);
+
+        _playerActions.Player_Map.Jump.performed += Jump_performed;
     }
 
     private void OnEnable()
@@ -26,9 +61,73 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        _moveInput = _playerActions.Player_Map.Movement.ReadValue<Vector2>();
-        _moveInput.y = 0f;
-        _rbody.velocity = _moveInput * _speed;
+        HandleMovement();
+        _rbody.velocity = new Vector2(_moveInput, _rbody.velocity.y);
+
+        HandleGravity();
+    }
+
+    private void HandleMovement()
+    {
+        _moveInput = _playerActions.Player_Map.Movement.ReadValue<float>() * _speed;
+    }
+
+    private void Jump_performed(InputAction.CallbackContext context)
+    {
+        if(IsGrounded())
+        {
+            _rbody.velocity += Vector2.up * _jumpPower;
+            _jumping = true;
+            StartCoroutine(EnableGroundCheckAfterJump());
+        }
+    }
+
+    private void HandleGravity()
+    {
+        if(_groundCheckEnabled && IsGrounded())
+        {
+            _jumping = false;
+        }
+        else if (_jumping && _rbody.velocity.y < 0f) //Jump Fall
+        {
+            _rbody.gravityScale = _initialGravityScale * _jumpFallGravityMultiplier;
+        }
+        else //Normal Fall
+        {
+            _rbody.gravityScale = _initialGravityScale;
+        }
+    }
+
+    private IEnumerator EnableGroundCheckAfterJump()
+    {
+        _groundCheckEnabled = false;
+        yield return _wait;
+        _groundCheckEnabled = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(_jumping)
+            Gizmos.color = Color.red;
+        else
+            Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(_boxCenter, _boxSize);
+    }
+
+    private bool IsGrounded()
+    {
+        //Center coordinate of box that checks if player is touching the ground
+        _boxCenter = new Vector2(_collider.bounds.center.x, _collider.bounds.center.y) +
+                     (Vector2.down * (_collider.bounds.extents.y + (_groundCheckHeight / 2f)));
+
+        //Size of the box (width, height)
+        _boxSize = new Vector2(_collider.bounds.size.x, _groundCheckHeight);
+
+        var groundBox = Physics2D.OverlapBox(_boxCenter, _boxSize, 0f, _groundMask);
+
+        if(groundBox != null)
+            return true;
+        return false;
     }
 
     private void OnDisable()
